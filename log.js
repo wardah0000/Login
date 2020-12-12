c6935c0f53898d6909d983b0adcc1df98ceb33d0
wardah0000 <wardah.aftab@gmail.com>
2020-12-12 19:00:08 +0500

    feat: Added Login page

---
+++ log.js
@@ -0,0 +1,21 @@
+
+
+function validate(){  
+    var name= document.getElementById("username").value;  
+    var pass= document.getElementById("password").value;
+
+if(name == null ||name == ""|| name.length <5){ 
+    alert("username must be atleast 5 characters long");
+    return false;
+}
+
+else if(pass == null || pass.length <8){
+    alert("password must be atleast 8 characters long");
+    return false;
+}
+
+else 
+    alert ("Logged In successfully");
+    return true;
+}
+
