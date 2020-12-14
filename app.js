c174209e7c8cf67b88956efbac6f2c7d466842bb
RafayGhafoor <rafayghafoor@pm.me>
2020-12-15 01:22:04 +0500

    fix: styling of login page

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-
-app.get('/login', function(req, res){
-    res.sendFile(path.join(__dirname+'/login.html'));
+app.use(express.static('public'))
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname + '/login.html'));
 });
 
 app.listen(port);
